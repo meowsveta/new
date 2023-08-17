08631c72e9e011d393c0902d5197854fc80fd09d
svetlana_klimova <sfettkoo@mail.ru>
2023-08-17 22:58:02 +0300

    done

--- 3/Task3.java
+++ 3/Task3.java
@@ -1,19 +1,22 @@
 public class Task3 {
 
     public static void main(String[] args) {
-        boolean result;
         String text = "Ной и вера - шанс у Сиона\n" +
                 "но Исус на шаре - Вийон";
-        String str1 = text.replaceAll("\\n", "").replaceAll("\\p{P}", "").replaceAll(" ", "").toLowerCase();
-        StringBuilder stringBuilder = new StringBuilder(str1).reverse();
 
-        if (str1.equals(stringBuilder.toString())){
+        if (palindrom(text)){
             System.out.println("Строка является палиндромом.");
-            result = true;
         }
         else {
             System.out.println("Строка не является палиндромом.");
-            result = false;
         }
+    }
+
+    public static boolean palindrom (String string){
+        boolean result;
+        String str1 = string.replaceAll("\\n", "").replaceAll("\\p{P}", "").replaceAll(" ", "").toLowerCase();
+        StringBuilder stringBuilder = new StringBuilder(str1).reverse();
+        result = str1.equals(stringBuilder.toString());
+        return result;
     }
 }
